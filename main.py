31844b11248affe918e2da133b555b89ee8f9d8d
Andrei-Ionuț IONESCU <andrei.ionescu1411@stud.acs.upb.ro>
2024-12-01 14:58:55 +0200

    Add dev loss

--- main.py
+++ main.py
@@ -129,6 +129,7 @@ def main(args: argparse.Namespace) -> None:
     metric_path = model_tag / "metrics"
     os.makedirs(metric_path, exist_ok=True)
     loss_history = []
+    dev_loss_history = []
 
     # Training
     for epoch in range(config["num_epochs"]):
@@ -136,54 +137,63 @@ def main(args: argparse.Namespace) -> None:
         running_loss = train_epoch(trn_loader, model, optimizer, device,
                                    scheduler, config)
         loss_history.append(running_loss)
-        produce_evaluation_file(dev_loader, model, device,
-                                metric_path/"dev_score.txt", dev_trial_path)
-        dev_eer, dev_tdcf = calculate_tDCF_EER(
-            cm_scores_file=metric_path/"dev_score.txt",
-            asv_score_file=database_path/config["asv_score_path"],
-            output_file=metric_path/"dev_t-DCF_EER_{}epo.txt".format(epoch),
-            printout=False)
-        print("DONE.\nLoss:{:.5f}, dev_eer: {:.3f}, dev_tdcf:{:.5f}".format(
-            running_loss, dev_eer, dev_tdcf))
-        writer.add_scalar("loss", running_loss, epoch)
-        writer.add_scalar("dev_eer", dev_eer, epoch)
-        writer.add_scalar("dev_tdcf", dev_tdcf, epoch)
+        print("Training loss: " + loss_history)
 
-        best_dev_tdcf = min(dev_tdcf, best_dev_tdcf)
-        if best_dev_eer >= dev_eer:
-            print("best model find at epoch", epoch)
-            best_dev_eer = dev_eer
-            torch.save(model.state_dict(),
-                       model_save_path / "epoch_{}_{:03.3f}.pth".format(epoch, dev_eer))
+        if epoch == 0 or epoch + 1 % 5 == 0:
+            dev_loss = produce_evaluation_file(dev_loader, model, device,
+                                    metric_path/"dev_score.txt", dev_trial_path)
+            dev_loss_history.append(dev_loss)
+            dev_eer, dev_tdcf = calculate_tDCF_EER(
+                cm_scores_file=metric_path/"dev_score.txt",
+                asv_score_file=database_path/config["asv_score_path"],
+                output_file=metric_path/"dev_t-DCF_EER_{}epo.txt".format(epoch),
+                printout=False)
+            print("DONE.\nLoss:{:.5f}, dev_eer: {:.3f}, dev_tdcf:{:.5f}".format(
+                running_loss, dev_eer, dev_tdcf))
+            writer.add_scalar("loss", running_loss, epoch)
+            writer.add_scalar("dev_eer", dev_eer, epoch)
+            writer.add_scalar("dev_tdcf", dev_tdcf, epoch)
 
-            # do evaluation whenever best model is renewed
-            if str_to_bool(config["eval_all_best"]):
-                produce_evaluation_file(eval_loader, model, device,
-                                        eval_score_path, eval_trial_path)
-                eval_eer, eval_tdcf = calculate_tDCF_EER(
-                    cm_scores_file=eval_score_path,
-                    asv_score_file=database_path / config["asv_score_path"],
-                    output_file=metric_path /
-                    "t-DCF_EER_{:03d}epo.txt".format(epoch))
+            best_dev_tdcf = min(dev_tdcf, best_dev_tdcf)
+            if best_dev_eer >= dev_eer:
+                print("best model find at epoch", epoch)
+                best_dev_eer = dev_eer
+                torch.save(model.state_dict(),
+                        model_save_path / "epoch_{}_{:03.3f}.pth".format(epoch, dev_eer))
 
-                log_text = "epoch{:03d}, ".format(epoch)
-                if eval_eer < best_eval_eer:
-                    log_text += "best eer, {:.4f}%".format(eval_eer)
-                    best_eval_eer = eval_eer
-                if eval_tdcf < best_eval_tdcf:
-                    log_text += "best tdcf, {:.4f}".format(eval_tdcf)
-                    best_eval_tdcf = eval_tdcf
-                    torch.save(model.state_dict(),
-                               model_save_path / "best.pth")
-                if len(log_text) > 0:
-                    print(log_text)
-                    f_log.write(log_text + "\n")
+                # do evaluation whenever best model is renewed
+                if str_to_bool(config["eval_all_best"]):
+                    produce_evaluation_file(eval_loader, model, device,
+                                            eval_score_path, eval_trial_path)
+                    eval_eer, eval_tdcf = calculate_tDCF_EER(
+                        cm_scores_file=eval_score_path,
+                        asv_score_file=database_path / config["asv_score_path"],
+                        output_file=metric_path /
+                        "t-DCF_EER_{:03d}epo.txt".format(epoch))
 
-            print("Saving epoch {} for swa".format(epoch))
-            optimizer_swa.update_swa()
-            n_swa_update += 1
-        writer.add_scalar("best_dev_eer", best_dev_eer, epoch)
-        writer.add_scalar("best_dev_tdcf", best_dev_tdcf, epoch)
+                    log_text = "epoch{:03d}, ".format(epoch)
+                    if eval_eer < best_eval_eer:
+                        log_text += "best eer, {:.4f}%".format(eval_eer)
+                        best_eval_eer = eval_eer
+                    if eval_tdcf < best_eval_tdcf:
+                        log_text += "best tdcf, {:.4f}".format(eval_tdcf)
+                        best_eval_tdcf = eval_tdcf
+                        torch.save(model.state_dict(),
+                                model_save_path / "best.pth")
+                    if len(log_text) > 0:
+                        print(log_text)
+                        f_log.write(log_text + "\n")
+
+                print("Saving epoch {} for swa".format(epoch))
+                optimizer_swa.update_swa()
+                n_swa_update += 1
+            writer.add_scalar("best_dev_eer", best_dev_eer, epoch)
+            writer.add_scalar("best_dev_tdcf", best_dev_tdcf, epoch)
+
+    with open("losses.txt", 'w') as file:
+        file.write(str(loss_history))
+        file.write('\n')
+        file.write(str(dev_loss_history))
 
     plt.plot(range(config["num_epochs"]), loss_history, marker='o', label="Training Loss")
     plt.xlabel("Epoch")
@@ -308,20 +318,29 @@ def produce_evaluation_file(
     save_path: str,
     trial_path: str) -> None:
     """Perform evaluation and save the score to a file"""
+    weight = torch.FloatTensor([0.1, 0.9]).to(device)
+    criterion = nn.CrossEntropyLoss(weight=weight)
     model.eval()
     with open(trial_path, "r") as f_trl:
         trial_lines = f_trl.readlines()
     fname_list = []
     score_list = []
+    total_loss = 0.0
+    num_batches = 0
     for batch_x, utt_id in tqdm(data_loader, desc="Processing batches"):
         batch_x = batch_x.to(device)
         batch_x = batch_x.to(device)
+        batch_y = batch_x.clone()
         with torch.no_grad():
-            _, batch_out = model(batch_x)
+            logits, batch_out = model(batch_x)
             batch_score = (batch_out[:, 1]).data.cpu().numpy().ravel()
         # add outputs
         fname_list.extend(utt_id)
         score_list.extend(batch_score.tolist())
+
+        batch_loss = criterion(logits, batch_y)  # Calculate batch loss
+        total_loss += batch_loss.item()
+        num_batches += 1
 
     assert len(trial_lines) == len(fname_list) == len(score_list)
     with open(save_path, "w") as fh:
@@ -330,6 +349,9 @@ def produce_evaluation_file(
             assert fn == utt_id
             fh.write("{} {} {} {}\n".format(utt_id, src, key, sco))
     print("Scores saved to {}".format(save_path))
+
+    average_loss = total_loss / num_batches if num_batches > 0 else 0.0
+    return average_loss
 
 
 def train_epoch(
